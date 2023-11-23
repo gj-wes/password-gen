b2dd776170e8da974e66b1e63695824e9aad9aea
gj-wes <negativegary@gmail.com>
2023-11-23 16:40:24 +0000

    Couple of missed symbols

--- src/App.vue
+++ src/App.vue
@@ -27,7 +27,7 @@ function generate() {
   const uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
   const lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
   const numbers = ['0','1','2','3','4','5','6','7','8','9'];
-  const symbols = ['+','-','/','*','<','>','~','(','[','!','@','£','#','$','%','&','?'];
+  const symbols = ['+','-','/','*','<','>','~','(',')','[',']','!','@','£','#','$','%','&','?','.',',','|','^'];
 
   // Combined character arrays based on checked settings
   let selected = []
